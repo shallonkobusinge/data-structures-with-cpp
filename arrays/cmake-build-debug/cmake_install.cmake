6641c1f4e3d1bdc84e10c622521a3584cd7900c2
Shallon Kobusinge <kobshallon041@gmail.com>
2022-04-27 10:06:14 +0200

    feat:ignore

--- arrays/cmake-build-debug/cmake_install.cmake
+++ arrays/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/kobsh/Documents/DSA/arrays
+# Install script for directory: C:/Users/kobsh/Documents/YEAR2/C++/PROGRAMS/arrays
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/kobsh/Documents/DSA/arrays/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/kobsh/Documents/YEAR2/C++/PROGRAMS/arrays/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
