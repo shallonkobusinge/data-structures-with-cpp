9a7b5f9723bb14dd85780fd94462b074731c0fb8
Shallon Kobusinge <kobshallon041@gmail.com>
2022-05-06 23:46:29 +0200

    Reverse text

---
+++ intro/Tasks/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+#include <stack>
+#include <string>
+#include <bits/stdc++.h>
+using namespace std;
+
+void reverseWord(string &word){
+    stack<string> reversed;
+    int low=0, high=0;
+    for(int i=0; i<word.length(); i++){
+        if(word[i] == ' '){
+            reversed.push(word.substr(low,high - low + 1));
+            low = high = i+ 1;
+        } else{
+            high = i;
+        }
+    }
+
+        reversed.push(word.substr(low));
+        word.clear();
+        while (!reversed.empty()){
+            word.append(reversed.top()).append(" ");
+            reversed.pop();
+        }
+        word.erase(prev(word.end()));
+
+
+}
+
+
+int main() {
+    string word;
+    cout<<" Enter the word to reverse "<<endl;
+    getline(cin,word);
+
+//    reverse(word.begin(),word.end());
+   reverseWord(word);
+   cout<<" "<<word<<endl;
+
+    return 0;
+}
