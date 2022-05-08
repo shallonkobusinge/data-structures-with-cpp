8d0f96670bd0689f7cd66921614c5c60ae703949
Shallon Kobusinge <kobshallon041@gmail.com>
2022-05-08 21:37:30 +0200

    reversing the word

--- intro/Tasks/main.cpp
+++ intro/Tasks/main.cpp
@@ -1,31 +1,34 @@
 #include <iostream>
-#include <algorithm>
 #include <stack>
 #include <string>
-#include <bits/stdc++.h>
 using namespace std;
 
+
+/*
+ *
+ * REVERSE A STRING WITHOUT REVERSING AN INDIVIDUAL WORD
+ *
+ * */
+
 void reverseWord(string &word){
-    stack<string> reversed;
-    int low=0, high=0;
-    for(int i=0; i<word.length(); i++){
-        if(word[i] == ' '){
-            reversed.push(word.substr(low,high - low + 1));
-            low = high = i+ 1;
-        } else{
-            high = i;
-        }
+   stack<string> reversed;
+   int low=0, high=0;
+   for(int i=0; i<word.length(); i++){
+       if(word[i] == ' '){
+           reversed.push(word.substr(low,high - low+1));
+           low = high = i+1;
+       }else{
+           high = i;
+       }
+   }
+    reversed.push(word.substr(low));
+    word.clear();
+
+    while (!reversed.empty()){
+        word.append(reversed.top()).append(" ");
+        reversed.pop();
     }
-
-        reversed.push(word.substr(low));
-        word.clear();
-        while (!reversed.empty()){
-            word.append(reversed.top()).append(" ");
-            reversed.pop();
-        }
-        word.erase(prev(word.end()));
-
-
+    word.erase(prev(word.end()));
 }
 
 
@@ -33,7 +36,6 @@ int main() {
     string word;
     cout<<" Enter the word to reverse "<<endl;
     getline(cin,word);
-
 //    reverse(word.begin(),word.end());
    reverseWord(word);
    cout<<" "<<word<<endl;
